72163d99f784c92c10a471048070f2c6037f49ea
MelPinDel <melpdel@gmail.com>
2022-08-27 19:37:29 +0800

    modify src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Banner from './components/melBanner';
 
 
 //react bootstrap
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Banner/>
   </React.StrictMode>
 );
 
